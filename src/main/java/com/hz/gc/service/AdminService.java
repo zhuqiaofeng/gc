fcc4fc141c389051e1748477a2b25b153d5e226e
gc <1225991593@qq.com>
2022-04-25 13:47:36 +0800

    zqf

--- src/main/java/com/hz/gc/service/AdminService.java
+++ src/main/java/com/hz/gc/service/AdminService.java
@@ -12,7 +12,5 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2022-04-23
  */
 public interface AdminService extends IService<Admin> {
-   public int select();
-   public int select2();
 
 }
