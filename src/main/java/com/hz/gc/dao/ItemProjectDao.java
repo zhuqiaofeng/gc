0ba10eb43e06d3b078eab07777750321f2fb4ff5
Warehouse <531495586@qq.com>
2022-05-04 18:00:41 +0800

    cyp

--- src/main/java/com/hz/gc/dao/ItemProjectDao.java
+++ src/main/java/com/hz/gc/dao/ItemProjectDao.java
@@ -3,6 +3,11 @@ package com.hz.gc.dao;
 import com.hz.gc.pojo.ItemProject;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
 /**
  * <p>
  *  Mapper 接口
@@ -11,6 +16,30 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @author 第三组
  * @since 2022-04-23
  */
+@Component
 public interface ItemProjectDao extends BaseMapper<ItemProject> {
+
+
+    //新  增
+
+    public int saveItemProject(ItemProject itemProject);
+
+    //根据id删除
+    public int deleteItemProjectById(Integer itemProjectId);
+//根据id修改
+    int updateItemProjectById(ItemProject itemProject);
+
+    //根据id查询
+    ItemProject findItemProjectById(Integer itemProjectId);
+
+    List<ItemProject> finditemProjectList(
+            @Param("pyl") Integer pyl,
+            @Param("page_size") Integer page_size,
+            @Param("itemProjectName") String itemProjectName,
+            @Param("itemProjectDesc") String itemProjectDesc);
+
+    Integer finditemProjectListCount(
+            @Param("itemProjectName") String itemProjectName,
+            @Param("itemProjectDesc") String itemProjectDesc);
 
 }
